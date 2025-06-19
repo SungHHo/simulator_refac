e5446c1c05fc143f2d9310a1b3463788fbfef369
SHLEE-235 <SHLEE-235@gmail.com>
2025-06-19 09:12:42 +0900

    0619-Simulator load config

---
+++ Simulator/Config/Config.h
@@ -0,0 +1,21 @@
+#ifndef MFR_CONFIG_H
+#define MFR_CONFIG_H
+
+#include <string>
+#include <termios.h>
+
+struct ConfigCommon
+{
+    int LSRecvPort = 0;    // Launch Simulator Port
+    std::string MFRSendIP; // Launch Controller IP
+    int MFRSendPort = 0;   // Launch Controller Port
+};
+
+// 공백 제거 및 문자열 처리 유틸 함수 선언
+std::string trim(const std::string &s);
+std::string toLower(const std::string &s);
+
+// ini 파일을 읽어 MfrConfig 구조체를 채움
+bool loadConfig(const std::string &filepath, ConfigCommon &config);
+
+#endif // MFR_CONFIG_H
